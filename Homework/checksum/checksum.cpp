4ad4dd6e6fbdc917df1f0d6dae8f7298e20a142e
guoyb17 <guoyb17@mails.tsinghua.edu.cn>
2019-11-24 18:30:49 +0800

    Finish puzzle 1 for mode 0.

--- Homework/checksum/checksum.cpp
+++ Homework/checksum/checksum.cpp
@@ -8,6 +8,21 @@
  * @return 校验和无误则返回 true ，有误则返回 false
  */
 bool validateIPChecksum(uint8_t *packet, size_t len) {
-  // TODO:
-  return true;
+  uint16_t header_len = packet[0] & 0xf;
+  header_len = header_len << 2;
+  uint16_t ori_checksum = packet[10];
+  ori_checksum = (ori_checksum << 8) + packet[11];
+  uint32_t cnt = 0;
+  for (uint16_t i = 0; i + 1 < header_len; i += 2) {
+    uint16_t tmp = i == 10 ? 0 : packet[i];
+    tmp = tmp << 8;
+    tmp += i == 10 ? 0 : packet[i + 1];
+    cnt += tmp;
+    while (0xffff < cnt) {
+      uint16_t tmps = cnt >> 16;
+      cnt = (cnt & 0xffff) + tmps;
+    }
+  }
+  cnt = ~cnt & 0xffff;
+  return cnt == ori_checksum;
 }
